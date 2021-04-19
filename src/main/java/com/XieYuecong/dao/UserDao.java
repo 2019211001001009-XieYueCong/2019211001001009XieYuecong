1d2a9b09db7477f6239311931ddda8b3fc65ff53
2019211001001009-XieYueCong <1569471415@qq.com>
2021-04-19 22:58:24 +0800

    HW-Week 7 Commit

---
+++ src/main/java/com/XieYuecong/dao/UserDao.java
@@ -0,0 +1,232 @@
+package com.XieYuecong.dao;
+
+import com.XieYuecong.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
+public class UserDao implements IUserDao{
+    @Override
+    public boolean saveUser(Connection con, User user) throws SQLException {
+        //insert ...into usertable
+        String sql="insert into usertable (id,username,password,email,gender,birthdate) values(?,?,?,?,?,?)";
+        PreparedStatement st=con.prepareStatement(sql);
+        //st.setString(1,id);
+        //st.setString(2,username);
+        //st.setString(3,password);
+        //st.setString(4,email);
+        //st.setString(5,gender);
+        // st.setString(6,birthdate);
+        ResultSet rs=st.executeQuery();
+       if(rs.next()){
+           user=new User();
+           user.setId(rs.getInt("id"));
+           user.setUsername(rs.getString("username"));
+           user.setPassword(rs.getString("password"));
+           user.setEmail(rs.getString("email"));
+           user.setGender(rs.getString("gender"));
+           user.setBirthDate(rs.getDate("birthdate"));
+           return true;
+       }else{
+           return false;
+       }
+    }
+
+    @Override
+    public int deleteUser(Connection con, User user) throws SQLException {
+        //delete...where id=?
+        String sql="delete from usertable where id=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        //st.setString(1,id);
+        ResultSet rs=st.executeQuery();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+        return 0;
+    }
+
+    @Override
+    public int updateUser(Connection con, User user) throws SQLException {
+        //update...where id=?
+        String sql="update usertable set id=? and username=? and password=? and email=? and gender=? and birthdate=?  where id=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+        return 0;
+    }
+
+    @Override
+    public User findById(Connection con, Integer id) throws SQLException {
+        //select ---where id=?
+        String sql="select * from usertable where id=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return user;
+    }
+
+    @Override
+    public User findByUsernamePassword(Connection con, String username, String password) throws SQLException {
+        //select ---where username=? and password=?
+        String sql="select * from usertable where username=? and password=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        st.setString(1,username);
+        st.setString(2,password);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            //get from rs and set into user model
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> findByUsername(Connection con, String username) throws SQLException {
+        String sql="select * from usertable where username=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+
+    @Override
+    public List<User> findByPassword(Connection con, String password) throws SQLException {
+        String sql="select * from usertable where password=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+
+    @Override
+    public List<User> findByEmail(Connection con, String email) throws SQLException {
+        String sql="select * from usertable where email=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+
+    @Override
+    public List<User> findByGender(Connection con, String gender) throws SQLException {
+        String sql="select * from usertable where gender=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+
+    @Override
+    public List<User> findByBirthdate(Connection con, Date birthDate) throws SQLException {
+        String sql="select * from usertable where birthdate=?";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+
+    @Override
+    public List<User> findAllUser(Connection con) throws SQLException {
+        //select * from usertable
+        String sql="select * from usertable";
+        PreparedStatement st=con.prepareStatement(sql);
+        ResultSet rs=st.executeQuery();
+        User user=null;
+        if(rs.next()){
+            user=new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthDate(rs.getDate("birthdate"));
+        }
+
+        return (List<User>) user;
+    }
+}
--- src/main/java/com/XieYuecong/week5/LoginServlet.java
+++ src/main/java/com/XieYuecong/week5/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.XieYuecong.week5;
 
+import com.XieYuecong.dao.UserDao;
+import com.XieYuecong.model.User;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -10,9 +13,10 @@ import java.sql.*;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
-    Connection con=null;
+    Connection con = null;
+
     @Override
-    public void init() throws ServletException{
+    public void init() throws ServletException {
         super.init();
         /*ServletContext Context=getServletContext();
         String driver=Context.getInitParameter("driver");
@@ -27,22 +31,47 @@ public class LoginServlet extends HttpServlet {
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }*/
-        con=(Connection) getServletContext().getAttribute("con");
+        con = (Connection) getServletContext().getAttribute("con");
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+        //doPost(request, response);
+        request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out=response.getWriter();
-        String username=request.getParameter("UserName");
-        String password=request.getParameter("Password");
-        System.out.println("hello world");
-        String sql="select * from usertable where username=? and password=? ";
+        PrintWriter out = response.getWriter();
+        //String id=request.getParameter("Id");
+        String username = request.getParameter("UserName");
+        String password = request.getParameter("Password");
+        //String email=request.getParameter("Email");
+        //String gender=request.getParameter("Gender");
+        //String birthdate=request.getParameter("BirthDate");
+
+        UserDao userDao=new UserDao();
+        try {
+            User user=userDao.findByUsernamePassword(con,username,password);
+            if(user!=null){
+                //有效的
+                request.setAttribute("user",user);//set user into request
+                request.getRequestDispatcher("WEB-INF/views/userInfo.jsp").forward(request,response);
+            }else{//无效的
+                request.setAttribute("message","Username or Password Error!!!");
+                request.getRequestDispatcher("WEB-INF/views/login.jsp").forward(request,response);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+
+
+
+        //System.out.println("hello world");
+      /*  String sql="select * from usertable where username=? and password=? ";
         PreparedStatement ps=null;
         try {
             ps=con.prepareStatement(sql);
@@ -69,6 +98,7 @@ public class LoginServlet extends HttpServlet {
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }
+        }*/
+
     }
 }
